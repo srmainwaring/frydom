6c75dda016f204206dbd371f7c33406044ea54e8
Pierre-Yves Wuillaume <pierre-yves.wuillaume@dice-engineering.com>
2020-06-24 13:11:33 +0200

    The IRF are only written if they are present.

--- tools/frydom/HDB5tool/pyHDB.py
+++ tools/frydom/HDB5tool/pyHDB.py
@@ -963,14 +963,16 @@ class pyHDB():
                                             "on body %u." % (j, body.i_body)
 
                 # Impulse response functions without forward speed.
-                dset = writer.create_dataset(irf_path + "/DOF_%u" % idof,
-                                        data=body.irf[:, 6*j+idof, :])
-                dset.attrs['Description'] = "Impulse response functions"
+                if(body.irf is not None):
+                    dset = writer.create_dataset(irf_path + "/DOF_%u" % idof,
+                                            data=body.irf[:, 6*j+idof, :])
+                    dset.attrs['Description'] = "Impulse response functions"
 
                 # Impulse response function with forward speed.
-                dset = writer.create_dataset(irf_ku_path + "/DOF_%u" % idof,
-                                        data=body.irf_ku[:, 6*j+idof, :])
-                dset.attrs['Description'] = "Impulse response functions Ku"
+                if(body.irf_ku is not None):
+                    dset = writer.create_dataset(irf_ku_path + "/DOF_%u" % idof,
+                                            data=body.irf_ku[:, 6*j+idof, :])
+                    dset.attrs['Description'] = "Impulse response functions Ku"
 
     def write_hydrostatic(self, writer, body, hydrostatic_path="/Hydrostatic"):
 
