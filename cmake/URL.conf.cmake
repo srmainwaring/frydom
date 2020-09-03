04ef698f4a1ae85a9a743c806a5e102f6d15b970
Lucas Letournel <lucas.letournel@dice-engineering.com>
2020-09-03 12:05:21 +0200

    [CMAKE] Add_fmt removed since already included via hermes-ce

--- cmake/URL.conf.cmake
+++ cmake/URL.conf.cmake
@@ -7,12 +7,6 @@ set(chrono_TAG 4.0.0)
 set(chrono_PATCH chrono_V4.0.0.patch)
 
 
-# fmt
-set(fmt_URL https://github.com/fmtlib/fmt.git)
-#set(fmt_TAG 4.1.0)
-set(fmt_TAG 6.0.0)
-
-
 # GeographicLib
 set(geographiclib_URL https://github.com/D-ICE/geographiclib.git)
 set(geographiclib_TAG v1.49)
