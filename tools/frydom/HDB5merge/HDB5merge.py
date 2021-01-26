cf99b1a3dcc949ac3a50da5fb6fb97f9d24e7fe9
Pierre-Yves Wuillaume <pierre-yves.wuillaume@dice-engineering.com>
2021-01-26 11:52:17 +0100

    Reading of hdb5 input files in hdb5merge.

--- tools/frydom/HDB5merge/HDB5merge.py
+++ tools/frydom/HDB5merge/HDB5merge.py
@@ -17,6 +17,8 @@
 import os
 import argparse
 
+import frydom.HDB5tool.HDB5 as H5T
+
 try:
     import argcomplete
 
@@ -34,7 +36,29 @@ def creation_parser():
 
 def get_parser(parser):
 
+    # Merge two hdb5 files into a single one.
+    parser.add_argument('--merge', '-m', nargs=2, action="store", help="""Reading a hdb5 file with the given name.""")
+
+    # Writing the hdb5 output file.
+    parser.add_argument('--write', '-w', action="store", help="""Writing the hdb5 output file with the given name.""")
+
     return parser
+
+def Read_hdb5(args):
+    """This function reads the input *.HDB5 files."""
+
+    # Reading a hdb5 file.
+    if (args.merge is not None):
+
+        # First hdb5 input file.
+        database_1 = H5T.HDB5()
+        database_1.read_hdb5(args.merge[0])
+
+        # Second hdb5 input file.
+        database_2 = H5T.HDB5()
+        database_2.read_hdb5(args.merge[1])
+
+    return database_1, database_2
 
 def main():
     ####################################################################################################################
@@ -48,6 +72,12 @@ def main():
         argcomplete.autocomplete(parser)
 
     args, unknown = parser.parse_known_args()
+
+    ####################################################################################################################
+    #                                        *.hdb5 input files to merge
+    ####################################################################################################################
+
+    database_1, database_2 = Read_hdb5(args)
 
 if __name__ == '__main__':
     main()
