fd3a25afaa31ebbda6b83b26040072e7d6c62439
Pierre-Yves Wuillaume <pierre-yves.wuillaume@dice-engineering.com>
2021-02-17 18:47:43 +0100

    body.position is not read with Nemoh.

--- tools/frydom/HDB5tool/bem_reader.py
+++ tools/frydom/HDB5tool/bem_reader.py
@@ -407,7 +407,7 @@ class NemohReader(_BEMReader):
                     point = list(map(float, dof[4:]))
 
                     # Center of gravity.
-                    body.point = point
+                    # body.position = point
 
                     if motion_type == 1: # Translation.
                         if(direction[0] == 1):
