eb56d9ba60f3bb1492e939daed8dc70f6a4da6a4
François Rongère <francois.rongere@dice-engineering.com>
2021-04-06 12:16:09 +0200

    Upgrading hermes to v1.4, hdb5_io to v2.2, mathutils to v1.6 and boost to >=1.66

--- cmake/URL.conf.cmake
+++ cmake/URL.conf.cmake
@@ -1,9 +1,9 @@
 
 # Here we declare the different PATH, TAG and PATCH to get the FRyDoM dependencies
 
-# mathutils
-set(mathutils_URL "https://github.com/projectchrono/chrono.git")
-set(mathutils_TAG v1.5 CACHE STRING "mathutils version")
+## mathutils
+#set(mathutils_URL "https://github.com/projectchrono/chrono.git")
+#set(mathutils_TAG v1.6 CACHE STRING "mathutils version")
 
 # Chrono
 set(chrono_URL "https://github.com/projectchrono/chrono.git")
@@ -12,7 +12,7 @@ set(chrono_PATCH chrono_V4.0.0.patch)
 
 
 # GeographicLib
-set(geographiclib_URL https://github.com/D-ICE/geographiclib.git)
+set(geographiclib_URL git://git.code.sf.net/p/geographiclib/code)
 set(geographiclib_TAG v1.49 CACHE STRING "geographiclib")
 set(geographiclib_PATCH GeographicLib.patch)
 
@@ -24,12 +24,12 @@ set(googletest_TAG release-1.8.1 CACHE STRING "googletest version")
 
 # hermes
 set(hermes_URL git@frydom-ce.org:ce/hermes.git)
-set(hermes_TAG v1.3.1 CACHE STRING " version")
+set(hermes_TAG v1.4 CACHE STRING " version")
 
 
 # HDB5_IO
 set(hdb5_io_URL git@frydom-ce.org:ce/hdb5-io.git)
-set(hdb5_io_TAG v2.1 CACHE STRING "hdb5_io version")
+set(hdb5_io_TAG v2.2 CACHE STRING "hdb5_io version")
 
 
 # TimeZone
@@ -58,16 +58,16 @@ set(cxxopts_URL https://github.com/jarro2783/cxxopts.git)
 set(cxxopts_TAG v2.2.0 CACHE STRING "cxxopts version")
 
 
-# HDF5
-set(HDF5_URL https://github.com/HDFGroup/hdf5.git)
-set(HDF5_TAG hdf5-1_8_21 CACHE STRING "HDF5 version")
+## HDF5
+#set(HDF5_URL https://github.com/HDFGroup/hdf5.git)
+#set(HDF5_TAG hdf5-1_10_6 CACHE STRING "HDF5 version")
 
 
 ### Indirect dependencies / already included
 
 # MathUtils
 set(mathutils_URL git@frydom-ce.org:ce/mathutils.git)
-set(mathutils_TAG v1.5 CACHE STRING "mathutils version")
+set(mathutils_TAG v1.6 CACHE STRING "mathutils version")
 set(MATHUTILS_BUILD_TESTS OFF CACHE BOOL "")
 set(MATHUTILS_BUILD_BOOST_TESTS OFF CACHE BOOL "")
 
@@ -83,5 +83,5 @@ set(MESHOUI_BUILD_TOOLS OFF CACHE BOOL "")
 
 
 # Boost
-set(boost_TAG 1.62)
+set(boost_TAG 1.66)
 
