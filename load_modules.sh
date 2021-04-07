37410770dd5facbcef524f0f40550e463033d619
Lucas Letournel <lucas.letournel@dice-engineering.com>
2021-04-07 17:08:37 +0200

    [WIP] modules to be added on LIGER in load_modules.sh

---
+++ load_modules.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+module load curl/7.52.1 zlib/1.2.11 gcc/7.4.0 cmake/3.16.5/gcc/8.3.0 boost/1.65.1/gcc-4.8.5-c7
+export CXX="/XF/Compilers/Gnu/8.3.0/Installed/bin/g++"
+export BOOST_ROOT="/XF/Libraries/Boost/1.65.1/Installed-c7/"
