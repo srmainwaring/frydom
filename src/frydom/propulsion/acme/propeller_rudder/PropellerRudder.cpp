4e0b6c3e78461fc1d62e4ee0564c4544a6800026
Francois Rongere <francois.rongere@dice-engineering.com>
2021-08-09 19:14:04 +0200

    WIP adding propeller rudder class

---
+++ src/frydom/propulsion/acme/propeller_rudder/PropellerRudder.cpp
@@ -0,0 +1,11 @@
+//
+// Created by frongere on 09/08/2021.
+//
+
+#include "PropellerRudder.h"
+
+namespace acme {
+
+
+
+}  // end namespace acme
---
+++ src/frydom/propulsion/acme/propeller_rudder/PropellerRudder.h
@@ -0,0 +1,14 @@
+//
+// Created by frongere on 09/08/2021.
+//
+
+#ifndef ACME_PROPELLERRUDDER_H
+#define ACME_PROPELLERRUDDER_H
+
+namespace acme {
+  class PropellerRudder {
+
+  };
+}  // end namespace acme
+
+#endif //ACME_PROPELLERRUDDER_H
