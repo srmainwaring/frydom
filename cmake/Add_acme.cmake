638389661fcd713a3e479e14ad7a688fe620e8ff
Francois Rongere <francois.rongere@dice-engineering.com>
2021-08-13 18:33:51 +0200

    acme has been totally exported into its own repository

---
+++ cmake/Add_acme.cmake
@@ -0,0 +1,18 @@
+
+include(FetchContent)
+
+FetchContent_Declare(acme
+        GIT_REPOSITORY ${acme_URL}
+        GIT_TAG ${acme_TAG}
+        )
+
+FetchContent_GetProperties(acme)
+if (NOT acme_POPULATED)
+    message(STATUS "******* FETCHING acme dependency from ${PROJECT_NAME} (requested version: ${acme_TAG}) *******")
+    FetchContent_Populate(acme)
+
+    # hdb5_io BUILD OPTIONS
+    set(acme_BUILD_TESTS OFF CACHE BOOL "")
+
+    add_subdirectory(${acme_SOURCE_DIR} ${acme_BINARY_DIR})
+endif ()
--- src/frydom/propulsion/acme/sail/sail.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by frongere on 06/08/2021.
-//
-
-#ifndef ACME_SAIL_H
-#define ACME_SAIL_H
-
-#endif //ACME_SAIL_H
